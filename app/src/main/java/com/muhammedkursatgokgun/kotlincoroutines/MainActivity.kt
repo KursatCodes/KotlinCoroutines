179791fc9b469b1ae63e03068a4a1f28302a2c96
Muhammed <kgokgun94@gmail.com>
2023-07-08 07:49:28 +0300

    First commit, Global scope created.

---
+++ app/src/main/java/com/muhammedkursatgokgun/kotlincoroutines/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.muhammedkursatgokgun.kotlincoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            launch {
+                println("selamünaleyküm")
+            }
+        }
+    }
+}
